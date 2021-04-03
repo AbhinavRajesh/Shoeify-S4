76a977e3dfd17a17dd28cad19ad3b1254740c6da
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-04-03 22:25:48 +0530

    Landing page kinda done

---
+++ HTML Templates/static/js/app.js
@@ -0,0 +1,78 @@
+const body = document.querySelector("body");
+const darkButton = document.getElementById("dark-mode");
+
+let container, camera, renderer, scene, shoe, light, controls;
+
+const init = () => {
+  container = document.querySelector(".home-right-container");
+
+  // Create Scene
+  scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 500;
+
+  // Camera Setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 5);
+
+  const ambient = new THREE.AmbientLight(0x404040, 1);
+  scene.add(ambient);
+
+  light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(-30, 30, 30);
+  light.castShadow = true;
+  scene.add(light);
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enablePan = false;
+  controls.enableZoom = false;
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.07;
+  controls.rotateSpeed = 0.7;
+
+  container.appendChild(renderer.domElement);
+
+  // Load Model
+  let loader = new THREE.GLTFLoader();
+  loader.load("../../assets/3d/scene.gltf", (gltf) => {
+    scene.add(gltf.scene);
+    shoe = gltf.scene.children[0];
+    shoe.rotation.x = -1.22;
+    shoe.rotation.y = -0.42;
+    shoe.rotation.z = 2;
+    animate();
+  });
+};
+
+let isInDiv = false;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  controls.update();
+  light.position.copy(camera.position);
+  renderer.render(scene, camera);
+};
+
+init();
+
+const onWindowResize = () => {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+};
+
+window.addEventListener("resize", onWindowResize);
+
+const toggleDarkMode = () => {
+  body.classList.toggle("dark");
+  darkButton.innerHTML = body.classList.contains("dark") ? "Light" : "Dark";
+};
